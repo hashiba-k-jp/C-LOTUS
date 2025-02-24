ce625ec9f31c5e13f72a3ad613756448fa639f51
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2025-02-24 16:55:27 +0900

    made as_class_list a public member (to access the list easily)

--- lotus.h
+++ lotus.h
@@ -17,10 +17,12 @@ using namespace std;
 
 class LOTUS{
 protected:
-    ASCLassList as_class_list;
     queue<Message> message_queue;
     vector<Connection> connection_list;
     map<ASNumber, vector<ASNumber>> public_aspa_list;
+
+public:
+    ASCLassList as_class_list;
 
 public:
     void add_AS(ASNumber asn){
