0ae391bc10ebd14d7f835a995c7a731148010672
hashiba-k-jp <hashiba.k.jp@gmail.com>
2025-03-06 17:26:23 +0900

    New main.cpp with security objects

--- main.cpp
+++ main.cpp
@@ -15,45 +15,16 @@
  * This main file is for test.
  */
 
-int main() {
-    LOTUS LOTUS;
-
-    /* IMPORT DATA FROM FILE */
-    // LOTUS.file_import("FILE_PATH");
-
-    /* GENERATE AS */
-    vector<ASNumber> as_number_list = {1, 10, 11, 12, 100, 200};
-    for(ASNumber& asn : as_number_list){
-        LOTUS.add_AS(ASNumber(asn));
-    }
-
-    /* CONNECT AS */
-    vector<tuple<ConnectionType, ASNumber, ASNumber>> connection_list = {
-        {ConnectionType::Down,   1,  10},
-        {ConnectionType::Peer,  10,  11},
-        {ConnectionType::Peer,  11,  12},
-        {ConnectionType::Down,  10, 100},
-        {ConnectionType::Down,  10, 200},
-        {ConnectionType::Down,   1, 200},
-    };
-    for(const auto& c : connection_list){
-        LOTUS.add_connection(get<0>(c), get<1>(c), get<2>(c));
-    }
-
-    /* ADD INIT MESSAGE TO ALL AS */
-    LOTUS.add_all_init();
-
-    /* ADD ASPA DATA */
-    LOTUS.add_ASPA(100, {10});
-
-    /* ENABLE ASPV */
-    LOTUS.set_ASPV(1, true, 1);
+void test_normal(LOTUS LOTUS){
+    std::cout << "/*-----------------------------------------------*/\n";
+    std::cout << "/*           attack test without security        */\n";
+    std::cout << "/*-----------------------------------------------*/\n";
 
     /* RUN LOTUS (before attack) */
     LOTUS.run();
 
     /* GENERATE ATTACK */
-    LOTUS.gen_attack(100, 1);
+    LOTUS.gen_attack(6, 1);
 
     /* RUN LOTUS (with attack) */
     LOTUS.run();
@@ -62,7 +33,107 @@ int main() {
     LOTUS.show_AS_list();
 
     /* EXPORT DATA TO FILE */
-    // LOTUS.file_export("FILE_PATH");
+    LOTUS.file_export("./NORMAL_attack.yml");
+
+    return;
+}
+
+void test_aspa(LOTUS LOTUS){
+    std::cout << "/*-----------------------------------------------*/\n";
+    std::cout << "/*           attack test with ASPA/ASPV          */\n";
+    std::cout << "/*-----------------------------------------------*/\n";
+
+    /* ADD ASPA DATA */
+    LOTUS.add_ASPA(1, {});
+    LOTUS.add_ASPA(3, {});
+    LOTUS.add_ASPA(4, {});
+
+    /* ENABLE ASPV */
+    LOTUS.set_ASPV(1, true, 1);
+    LOTUS.set_ASPV(3, true, 1);
+    LOTUS.set_ASPV(4, true, 1);
+
+    /* RUN LOTUS (before attack) */
+    LOTUS.run();
+
+    /* GENERATE ATTACK */
+    // LOTUS.gen_attack(src, tar);
+    IPAddress target_address = LOTUS.get_AS(1)->network_address;
+    LOTUS.add_messages(MessageType::Update, 6, 3, target_address, Path{1, 2, 6});
+
+    /* RUN LOTUS (with attack) */
+    LOTUS.run();
+
+    /* SHOW ALL AS INFO */
+    LOTUS.show_AS_list();
+
+    /* EXPORT DATA TO FILE */
+    LOTUS.file_export("./ASPA_attack.yml");
+
+    return;
+}
+
+void test_isec(LOTUS LOTUS){
+    std::cout << "/*-----------------------------------------------*/\n";
+    std::cout << "/*           attack test with BGP-iSec           */\n";
+    std::cout << "/*-----------------------------------------------*/\n";
+
+    /* ENABLE BGP-ISEC */
+    LOTUS.switch_adoption_iSec(1, true, 1);
+    LOTUS.switch_adoption_iSec(3, true, 1);
+    LOTUS.switch_adoption_iSec(4, true, 1);
+
+    /* ADD PROCONID LIST */
+    LOTUS.add_ProConID_all();
+
+    /* RUN LOTUS (before attack) */
+    LOTUS.run();
+
+    /* GENERATE ATTACK */
+    // LOTUS.gen_attack(src, tar);
+    IPAddress target_address = LOTUS.get_AS(1)->network_address;
+    LOTUS.add_messages(MessageType::Update, 6, 3, target_address, Path{1, 2, 6});
+
+    /* RUN LOTUS (with attack) */
+    LOTUS.run();
+
+    /* SHOW ALL AS INFO */
+    LOTUS.show_AS_list();
+
+    /* EXPORT DATA TO FILE */
+    LOTUS.file_export("./ISEC_attack.yml");
+
+    return;
+}
+
+int main() {
+    LOTUS LOTUS;
+
+    /* IMPORT DATA FROM FILE */
+    // LOTUS.file_import("FILE_PATH");
+
+    /* GENERATE AS */
+    LOTUS.add_AS(ASNumber{1});
+    LOTUS.add_AS(ASNumber{2});
+    LOTUS.add_AS(ASNumber{3});
+    LOTUS.add_AS(ASNumber{4});
+    LOTUS.add_AS(ASNumber{5});
+    LOTUS.add_AS(ASNumber{6});
+
+    /* CONNECT AS */
+    LOTUS.add_connection(ConnectionType::Down, ASNumber{4}, ASNumber{2});
+    LOTUS.add_connection(ConnectionType::Down, ASNumber{4}, ASNumber{6});
+    LOTUS.add_connection(ConnectionType::Down, ASNumber{3}, ASNumber{5});
+    LOTUS.add_connection(ConnectionType::Down, ASNumber{3}, ASNumber{6});
+    LOTUS.add_connection(ConnectionType::Down, ASNumber{2}, ASNumber{1});
+    LOTUS.add_connection(ConnectionType::Peer, ASNumber{3}, ASNumber{4});
+
+    /* ADD INIT MESSAGE TO ALL AS */
+    LOTUS.add_all_init();
+
+    test_normal(LOTUS);
+    test_aspa(LOTUS);
+    test_isec(LOTUS);
 
     return 0;
 }
